2d6b613ca8f5f4905108ddb03e9b458e9fb948d3
Reyan9450 <reyans9450@gmail.com>
2025-10-23 13:50:11 +0530

    api chnaged

--- Frontend/src/services/weatherServices.js
+++ Frontend/src/services/weatherServices.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchMetars = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/api/metars`);
+    const response = await axios.get(`https://vayumet-weather-server.onrender.com/api/metars`);
     return response.data;
   } catch (error) {
     console.error("Error fetching METAR data:", error);
@@ -14,7 +14,7 @@ export const fetchMetars = async () => {
 
 export const fetchTafs = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/api/tafs`);
+    const response = await axios.get(`https://vayumet-weather-server.onrender.com/api/tafs`);
     return response.data;
   } catch (error) {
     console.error("Error fetching TAF data:", error);
