076b8eec04d2d2fe4bceefc86debbcca256c9b2f
Reyan9450 <reyans9450@gmail.com>
2025-11-06 09:12:16 +0530

    bounding box technique has been implemented

--- Backend/routes/metar.routes.js
+++ Backend/routes/metar.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { getAllMetars, getMetarByStationId, getMetarByStationName } from '../controllers/metar.controller.js';
+import { getAllMetars, getMetarByStationId, getMetarByStationName, getMetarsByBbox } from '../controllers/metar.controller.js';
 
 const router = express.Router();
-
+router.route('/mapdata').get(getMetarsByBbox);
 router.route('/').get(getAllMetars);
 router.route('/name/:stationName').get(getMetarByStationName);
 router.route('/:stationId').get(getMetarByStationId);
--- Frontend/src/services/weatherServices.js
+++ Frontend/src/services/weatherServices.js
@@ -1,21 +1,37 @@
 import axios from 'axios';
 
+/**
+ * Fetches METAR data for a specific bounding box
+ * @param {string} bbox - The bounding box string "north,west,south,east"
+ */
 
 // Fetches all METAR data from your backend
-export const fetchMetars = async () => {
+export const fetchMetars = async (bbox) => {
+    if (!bbox) {
+        console.log("No bbox provided for fetchMetars, returning empty array.");
+        return [];
+    }
     try {
-        const response = await axios.get('/api/metars');
+        const response = await axios.get(`/api/metars/mapdata?bbox=${bbox}` );
+        // console.log("METAR data fetched for bbox:",response.data);
         return response.data;
     } catch (error) {
         console.error("Error fetching METAR data:", error);
         return []; // Return an empty array on error
     }
 };
-
+/**
+ * Fetches TAF data for a specific bounding box
+ * @param {string} bbox - The bounding box string "north,west,south,east"
+ */
 // Fetches all TAF data from your backend
-export const fetchTafs = async () => {
+export const fetchTafs = async (bbox) => {
+    if (!bbox) {
+        console.log("No bbox provided for fetchTafs, returning empty array.");
+        return [];
+    }
     try {
-        const response = await axios.get('/api/tafs');
+        const response = await axios.get(`/api/tafs/mapdata?bbox=${bbox}` );
         return response.data;
     } catch (error) {
         console.error("Error fetching TAF data:", error);
